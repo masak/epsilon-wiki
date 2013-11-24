e9d5f12c560451ff547723dc471ac69ec4ec377f
Carl Masak <cmasak@gmail.com>
2013-11-24 16:29:18 +0100

    first commit
    Just static content.

---
+++ wiki.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <title>Carl Mäsak</title>
+</head>
+<body>
+  <h1>Carl Mäsak</h1>
+
+  <p>Developer, open source advocate, programming instructor, Perl 6 core
+  contributor.</p>
+
+  <p><a
+  href="http://www.hitta.se/carl%20m%C3%A4sak/f%C3%B6retag_och_personer">Phone
+  and address</a>.</p>
+
+  <p>I work for <a
+  href="http://edument.se/consultingservices/consultant/carl-masak">Edument</a>.</p>
+
+  <p>I <a href="http://strangelyconsistent.org/">blog</a>.</p>
+</body>
