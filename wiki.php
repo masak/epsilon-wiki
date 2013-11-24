177489a39a1c6bf771bd4cfe965eb6ec16a840f0
Carl Masak <cmasak@gmail.com>
2013-11-24 16:58:56 +0100

    render HTML from Markdown
    At read time.

--- wiki.php
+++ wiki.php
@@ -1,20 +1,22 @@
+<?php
+# Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class){
+    require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+
+use \Michelf\Markdown;
+
+$text = file_get_contents('articles-md/0.md');
+$html = Markdown::defaultTransform($text);
+
+?>
 <!DOCTYPE html>
 <head>
   <meta charset="utf-8">
   <title>Carl Mäsak</title>
 </head>
 <body>
-  <h1>Carl Mäsak</h1>
-
-  <p>Developer, open source advocate, programming instructor, Perl 6 core
-  contributor.</p>
-
-  <p><a
-  href="http://www.hitta.se/carl%20m%C3%A4sak/f%C3%B6retag_och_personer">Phone
-  and address</a>.</p>
-
-  <p>I work for <a
-  href="http://edument.se/consultingservices/consultant/carl-masak">Edument</a>.</p>
-
-  <p>I <a href="http://strangelyconsistent.org/">blog</a>.</p>
+    <?php
+        echo $html;
+    ?>
 </body>
